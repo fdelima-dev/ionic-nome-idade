189423eaad56da27d4b44a63d1333ee4a8c3b437
Felipe-Developer827 <felipelimaciencia@gmail.com>
2026-05-16 10:10:37 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component } from "@angular/core"
+import { IonicModule } from "@ionic/angular"
+import { CommonModule } from "@angular/common"
+import { FormsModule } from "@angular/forms"
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
 })
-export class HomePage {
-  constructor() {}
+
+export class HomePage{
+  nome: string = '';
+  idade: number = 0;
+  resultado: string = '';
+
+  mostrar(){
+    this.resultado = `Você se chama ${this.nome} e tem ${this.idade} anos`
+  }
 }
